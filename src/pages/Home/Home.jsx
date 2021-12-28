8f00ec8e0d9601bf4621cf0ed1ec6c8a133dbb96
rajveersingh000 <rajveersinghbbu@gmail.com>
2021-12-28 16:56:38 +0530

    add usestate hook

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar"
 
@@ -7,14 +7,37 @@ import Navbar from "../../components/Navbar/Navbar"
 function Home() {
 
     console.log("Tut");
-
     const person = "Shivam";
-    
+
+    const [avenger, setAvenger] = useState("Doctor Strange");
+    console.log(avenger);
+
+    const [card, setCard] = useState([{
+        title: "",
+        body: ""
+    }]);
+
+    function handleCards(prevValue) {
+        // Spread operator => ...
+        setCard([...prevValue, {
+            title: "qwerty",
+            body: "body"
+        }])
+    }
+    console.log(card);
+
+
     return (
         <div>
             <Navbar />
             <h2 className="heading">rejggd</h2>
             <h3>Hello, {person}</h3>
+
+            <h2>{avenger}</h2>
+            <button onClick={() => {setAvenger("Sorcerer Supreme")}}>Change Name</button>
+
+            {/* <h2>{card}</h2> */}
+            <button onClick={() => {handleCards(card)}}>Change</button>
         </div>
     )
 }
